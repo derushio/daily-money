acebe825ea8c6f0c92f2df3ca97b55435c675cc8
derushio <narumi18wa@gmail.com>
2018-12-09 21:43:41 +0900

    <>[new file] init

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/src/stylus/app.styl';
+import InstantVuetify from 'instant-vuetify-overlays';
+
+Vue.use(Vuetify, {
+    iconfont: 'mdi',
+});
+Vue.use(InstantVuetify);
