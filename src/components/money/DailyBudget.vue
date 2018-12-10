4e056da82c724fd6aec13cb5c6cab451096ec30e
derushio <narumi18wa@gmail.com>
2018-12-10 22:42:47 +0900

    <master>[modified] fix margin

--- src/components/money/DailyBudget.vue
+++ src/components/money/DailyBudget.vue
@@ -1,9 +1,9 @@
 <template lang='pug'>
 .daily-budget
-    h2.headline {{ index }} の情報
+    h2.headline: p {{ index }} の情報
 
     v-container.month
-        h3.title 月の予算
+        h3.title: p 月の予算
         v-layout(wrap row align-center)
             v-flex(lg3 sm4 xs6): v-text-field(:value='month.budget' readonly)
             span 円
@@ -12,7 +12,7 @@
     v-divider
 
     v-container.daily
-        h3.title １日あたりの予算
+        h3.title: p １日あたりの予算
         v-layout(wrap row align-center)
             v-flex(lg3 sm4 xs6): v-text-field(:value='getDailyBudget()' readonly)
             span 円
@@ -21,6 +21,17 @@
     v-container.actions
         v-layout(wrap row)
             v-btn(@click='addAction') 収支を追加
+    v-divider
+
+    v-container.actions
+        h3.title: p 収支リスト
+        v-layout(wrap row)
+            v-flex(v-for='action in month.actions' lg4 sm6 xs12).pa-1: v-card
+                v-card-text: v-layout(wrap row)
+                    span {{ action.name }}
+                    v-divider(vertical).mx-2
+                    span {{ action.value }}
+                    span 円
     v-divider
 </template>
 
